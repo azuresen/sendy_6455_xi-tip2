37741ad344538e9b07d94a7a709037f0f4a61cfe
azuresen <gozalisendygozali@gmail.com>
2023-08-08 21:20:01 +0700

    jobsheet

---
+++ latihan/properti_string_lenght.js
@@ -0,0 +1,12 @@
+// length 
+const readline = require(`readline`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+} );
+
+rl.question("Masukan sebuah kalimat: ", (inputString) => {
+    // Contoh property lenght
+    console.log(`Panjang kalimat: ${inputString.length}`);
+    rl.close();
+});
